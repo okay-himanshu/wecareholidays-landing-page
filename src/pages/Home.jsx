b213b1da653be872ebbbb46fffe0b44d453acef4
amit97s <amit1397singh@gmail.com>
2024-03-19 17:53:51 +0530

    grid added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import Banner from "../components/Banner";
+import Grid from "../components/Grid";
 
 function Home() {
-  return <div><Banner/></div>;
+  return <div><Banner/>
+  <Grid/>
+  </div>;
 }
 
 export default Home;
